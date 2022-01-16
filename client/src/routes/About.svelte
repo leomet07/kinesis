917f4722994eaeafc511faf76459860a2e855923
leomet07 <leomet07@gmail.com>
2022-01-15 23:34:59 -0500

    Credits!

--- client/src/routes/About.svelte
+++ client/src/routes/About.svelte
@@ -2,7 +2,8 @@
 	<h1 id="title">About</h1>
 	<p class="explain">
 		Kinesis is a novel method of managing covid contact tracing, written for <a
-			href="https://stuyhacks-xi.devpost.com/"><i>StuyHacks XI</i></a
+			href="https://stuyhacks-xi.devpost.com/"
+			target="_blank"><i>StuyHacks XI</i></a
 		>. Instead of guessing where people are based on involuntary geolocation
 		and wireless info, this app uses voluntary (but safe) information.
 		Rather than force people into being silently tracked, we offer them the
@@ -11,21 +12,48 @@
 		software libraries to provide consistent and reliable service, such as
 		e-mail notifcatications to potentially exposed indivudiduals.
 	</p>
+	<p class="explain" id="source_code">
+		<a href="https://github.com/leomet07/kinesis/" target="_blank"
+			>Source Code</a
+		>
+	</p>
+	<div id="contributors">
+		<h3>Contributors:</h3>
+		<p>
+			<a href="https://github.com/leomet07/" target="_blank"
+				>Lenny M (@leomet07)</a
+			>
+		</p>
+		<p>
+			<a href="https://github.com/TheEgghead27" target="_blank"
+				>David C (@TheEgghead27)</a
+			>
+		</p>
+		<p>
+			<a href="https://github.com/thegu5" target="_blank"
+				>Gus W (@thegu5)</a
+			>
+		</p>
+	</div>
 </main>
 
 <style>
 	#about {
 		text-align: center;
+		padding-bottom: 200px;
 	}
 	.explain {
-		text-align: left;
 		width: 70vw;
 		margin: auto;
 		font-size: 20px;
+		margin-bottom: 10px;
 	}
+	#contributors {
+		font-size: 18px;
+	}
+
 	@media only screen and (max-width: 600px) {
 		.explain {
-			text-align: center;
 			width: 95vw;
 		}
 	}
