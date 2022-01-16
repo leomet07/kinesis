2b14e74613e79d0fb5fbf6c9c15bd405f49dcc5f
leomet07 <leomet07@gmail.com>
2022-01-15 23:19:08 -0500

    About page fix

--- client/src/routes/About.svelte
+++ client/src/routes/About.svelte
@@ -1,10 +1,32 @@
-<h1>About</h1>
-<p>
-	Kinesis is a novel method of managing contact tracing. I, written for <a href="https://stuyhacks-xi.devpost.com/"><code>StuyHacks XI</code></a>nstead of guessing
-	where people are based on involuntary geolocation and wireless
-	finWe do not  our system achieves precision by tying ; everyone is transparently offeredyste service and certainty of contact tracoupled with theed with the	to our contact tracing indices. Rather than force people into being silently
-such as e-mail notifcatications to potentially sexposed indivudiduals.	tracked, we offer them the service and certainty of contact tracing combined
-	with the convenience of seating arrangements made in advalidate_component.
-	We use industry standard software libraries to provide consistent and
-	reliable service,
-</p>
+<main id="about">
+	<h1 id="title">About</h1>
+	<p class="explain">
+		Kinesis is a novel method of managing covid contact tracing, written for <a
+			href="https://stuyhacks-xi.devpost.com/"><i>StuyHacks XI</i></a
+		>. Instead of guessing where people are based on involuntary geolocation
+		and wireless info, this app uses voluntary (but safe) information.
+		Rather than force people into being silently tracked, we offer them the
+		service and certainty of contact tracing combined with the convenience
+		of seating arrangements and reservations. We use industry standard
+		software libraries to provide consistent and reliable service, such as
+		e-mail notifcatications to potentially exposed indivudiduals.
+	</p>
+</main>
+
+<style>
+	#about {
+		text-align: center;
+	}
+	.explain {
+		text-align: left;
+		width: 70vw;
+		margin: auto;
+		font-size: 20px;
+	}
+	@media only screen and (max-width: 600px) {
+		.explain {
+			text-align: center;
+			width: 95vw;
+		}
+	}
+</style>
